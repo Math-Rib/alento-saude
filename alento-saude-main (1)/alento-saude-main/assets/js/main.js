e175105e43a1afcf1794c1c80d246533b7652727
Christian26-cry <chriskayke01@gmail.com>
2026-04-24 19:58:47 -0300

    Adicionando contato

---
+++ alento-saude-main (1)/alento-saude-main/assets/js/main.js
@@ -0,0 +1,9 @@
+import { initDarkMode } from "./global/dark-mode.js";
+import { initIndex } from "./pages/index.js";
+import {initHomePaciente} from "./pages/home-paciente.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    initDarkMode();
+    initIndex();
+    initHomePaciente();
+});
