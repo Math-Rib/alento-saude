1585298cd8c6ebfe190538f74c15c1d0aaa1034c
Math <matheusribsouza13@gmail.com>
2026-04-29 14:16:01 -0300

    Arquivos cadastro.html e main.js ajustados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import { initDarkMode } from "./global/dark-mode.js";
 import { initIndex } from "./pages/index.js";
-import { initHomePaciente } from "./pages/home-paciente.js";
+import { initHomePaciente } from "./pages/home_paciente.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initDarkMode();
